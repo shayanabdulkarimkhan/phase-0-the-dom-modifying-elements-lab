4722ada0417d755f35dc3c82c4b07434ed992457
shayan <sak704@nyu.edu>
2022-03-06 12:46:20 -0800

    Completed Lab

--- index.js
+++ index.js
@@ -1,61 +1,13 @@
-const element = document.createElement("div");
+// Write your code here!
 
-document.body.append(element)
+const main = document.querySelector("main");
+console.log(main);
+main.remove();
 
-const ul = document.createElement("ul");
-
-for (let i = 0; i < 3; i++) {
-  const li = document.createElement("li");
-  li.textContent = (i + 1).toString();
-  ul.append(li);
-}
-
-element.append(ul);
-
-li.textContent = "Hi there!";
-
-console.log(li.textContent);
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
 
 
+newHeader.textContent = "YOUR-NAME is the champion";
 
-const main = document.getElementById("main");
-main.innerHTML =
-  "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
-'<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>'
-<main id="main">
-  <h1>Poodles!</h1>
-  <h3>
-    An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a
-    post-Frankfurt School Appropriationist Perspective
-  </h3>
-  <p><em>By: Byron Q. Poodle, Esq., BA.</em></p>
-</main>
-
-const element = document.getElementById("main");
-element.style.height = "300px";
-element.style.backgroundColor = "#27647B";
-
-element.textContent = "You've changed what's on the screen!";
-
-element.style.fontSize = "24px";
-element.style.marginLeft = "30px";
-element.style.lineHeight = 2;
-
-element.className = "pet-listing dog";
-
-<main id="main" class="pet-listing dog"></main>
-
-element.classList.remove("dog");
-element.classList.add("cat", "sale");
-undefined
-<main id="main" class="pet-listing cat sale"></main>
-
-someElement.removeChild(someChildElement);
-
-(anonymous) @ VM374:1
-const ul = document.getElementsByTagName("ul")[0];
-const secondChild = ul.querySelector("li:nth-child(2)");
-ul.removeChild(secondChild);
-<li>​2​</li>​
-ul.remove();
-undefined// Write your code here!
+document.body.append(newHeader);
